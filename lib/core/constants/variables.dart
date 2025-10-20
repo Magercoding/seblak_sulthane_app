bea650964bb7f7fb5ab01ff3d75514eb232185cd
Bintang Fabian <bintangalfin33@gmail.com>
2025-10-20 17:50:40 +0700

    Fix: Gradle Version for Build Apk

--- lib/core/constants/variables.dart
+++ lib/core/constants/variables.dart
@@ -2,4 +2,5 @@ class Variables {
   static const String appName = 'POS Kasir Resto App';
   static const String apiVersion = 'v1';
   static const String baseUrl = 'https://api.seblaksulthane.com';
+  // static const String baseUrl = 'http://10.0.2.2:8000';
 }
